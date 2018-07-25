a014bbec28520108e2a53a6145e46b479e801fd6
Ryan <ryan.elr@outlook.com>
2018-07-25 14:30:50 +0200

    ex8

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 8 parti 1 php</title>
+  </head>
+  <body>
+    <?php
+    $addition = 3 + 4;
+    $multiplication = 5 * 20;
+    $division = 45 / 5;
+    echo $addition.'<br/>';
+    echo $multiplication.'<br/>';
+    echo $division.'<br/>';
+    ?>
+  </body>
+</html>
